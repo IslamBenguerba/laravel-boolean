29a1dc503a666da71af0ad165ea6981549e08ad1
Islam.Benguerba <islam.benguerba@gmail.com>
2023-10-19 17:39:33 +0200

    Do Table

--- database/migrations/2023_10_19_144045_create_cocktails_table.php
+++ database/migrations/2023_10_19_144045_create_cocktails_table.php
@@ -13,6 +13,28 @@ return new class extends Migration
     {
         Schema::create('cocktails', function (Blueprint $table) {
             $table->id();
+            $table->string('idDrink')->nullable();
+            $table->string('strDrink');
+            $table->string('strDescrizione')->nullable();
+            $table->string('strCategorstry')->nullable();
+            $table->string('strIBA')->nullable();
+            $table->string('strGlastrss')->nullable();
+            $table->string('strInstructions')->nullable();
+            $table->string('strInstructionsES')->nullable();
+            $table->string('strInstructionsDE')->nullable();
+            $table->string('strInstructionsFR')->nullable();
+            $table->string('strInstructionsIT')->nullable();
+            $table->string('strDrinkThumb')->nullable();
+            for ($i = 1; $i < 16; $i++) {
+                $table->string('strIngredient'.$i)->nullable();
+            }
+            for ($i = 1; $i < 16; $i++) {
+                $table->string('strMeasure'.$i)->nullable();
+            }
+            $table->string('strImageSource')->nullable();
+            $table->string('strImageAttribution')->nullable();
+            $table->string('strCreativeCommonsConfirmed')->nullable();
+            $table->date('dateModified')->nullable();
             $table->timestamps();
         });
     }
