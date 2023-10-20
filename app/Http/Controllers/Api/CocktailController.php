7bc316fc4778c960c02c0ed8929148ac12a40d89
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-10-20 17:03:51 +0200

    add api route & api controller

---
+++ app/Http/Controllers/Api/CocktailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
+use Illuminate\Http\Request;
+
+class CocktailController extends Controller
+{
+    public function index(){
+        //recupero i dati dalla tabella
+        $cocktails = Cocktail::all();
+        dd($cocktails);
+
+
+        //ritorno il json con i dati
+        return response()->json($cocktails);
+    }
+}
